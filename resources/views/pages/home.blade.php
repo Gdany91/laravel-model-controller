827212c04d02fb0a5b2d07286e0dc962b22f0322
gdany91 <you@example.comdgarofalo52@gmail.com>
2023-01-31 17:02:01 +0100

    stampa

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,11 +2,16 @@
 
 @section('content')
     <h1>MOVIES</h1>
-    <div id="test">
-        <ul>
+    <div >
+        <ul >
             @foreach ($movies as $movie)
-                <li>
-                    Titolo {{ $movie -> title }} - Titolo originale {{ $movie -> original_title }} Voto {{ $movie -> vote }} </li>
+                <li >
+                    Titolo: {{ $movie -> title }}   -
+                    Titolo originale: {{ $movie -> original_title }} - 
+                    Nationality: {{ $movie -> nationality }} - 
+                    Date: {{ $movie -> date }} -  
+                    Voto: {{ $movie -> vote }} 
+                </li>
             @endforeach
         </ul>
     </div>
